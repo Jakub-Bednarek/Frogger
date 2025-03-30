52a52ea6cf398b67d14294f431e392b77ce1929f
Jakub-Bednarek <jakub.bednare1009@gmail.com>
2025-03-30 15:26:24 +0200

    Content cleanup pt 1

--- Source/Frogger/Frogger.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Frogger.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Frogger, "Frogger" );
- 
