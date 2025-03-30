b82447de176e4a736a1a8f117641ef229a7a657d
Jakub-Bednarek <jakub.bednare1009@gmail.com>
2025-03-30 15:30:25 +0200

    Directories cleanup for git pt2

---
+++ Source/Frogger/Frogger.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Frogger.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Frogger, "Frogger" );
+ 
